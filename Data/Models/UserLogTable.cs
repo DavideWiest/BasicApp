306907ae73878bea8b666b2e2b19a223924e9e1b
Davide R. Wiest <davide.wiest2@gmail.com>
2023-08-31 14:28:16 +0200

    Update UserLogTable.cs

--- Data/Models/UserLogTable.cs
+++ Data/Models/UserLogTable.cs
@@ -1,5 +1,42 @@
 ﻿namespace basicApp.Data.Models;
 
-public class UserLogTable
+using System;
+using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
+
+public class Notification
 {
+    [Key]
+    public int NotificationId { get; set; }
+
+    public int UserId { get; set; }
+    
+    public string Message { get; set; }
+
+    public DateTime Timestamp { get; set; }
+
+    public NotificationStatus Status { get; set; }
+
+    public NotificationPriority Priority { get; set; }
+
+    public string Source { get; set; }
+
+    public string Link { get; set; }
+
+    public string Metadata { get; set; }
+
+    public DateTime? ExpirationDate { get; set; }
+}
+
+public enum NotificationStatus
+{
+    Unread,
+    Read
+}
+
+public enum NotificationPriority
+{
+    Low,
+    Medium,
+    High
 }
