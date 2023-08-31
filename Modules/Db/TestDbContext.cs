2460e5ee370e0b40ba48b6c4ce470f38ee225617
Davide Wiest <davide.wiest2@gmail.com>
2023-08-31 19:47:32 +0200

    edit ef files

---
+++ Modules/Db/TestDbContext.cs
@@ -0,0 +1,53 @@
+﻿namespace BasicApp.Modules.Db;
+
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using BasicApp.Data.Models;
+
+
+public class TestDbContext : DbContext
+{
+    public DbSet<TestUser> TestUsers { get; set; }
+
+
+    public DbSet<UserNotification> UserNotifications { get; set; }
+
+
+    public TestDbContext(string ConnectionString) : base(ConnectionString)
+    {
+        
+    }
+
+    public TestDbContext() : base(GetConnectionString())
+    {
+        
+    }
+
+    public static string GetConnectionString()
+    {
+        var configBuilder = new ConfigurationBuilder()
+        .AddJsonFile("config/appsettings.json")
+        .AddJsonFile("config/appsettings.Development.json")
+        .Build();
+
+#if DEBUG
+        string ConnectionString = configBuilder["Database:ConnectionStringTesting"]!;
+#else
+        string ConnectionString  = configBuilder["Database:ConnectionStringProduction"]!;
+#endif
+        return ConnectionString;
+    }
+
+    protected override void OnModelCreating(DbModelBuilder modelBuilder)
+    {
+        modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<TestUser>().ToTable("TestUser");
+        modelBuilder.Entity<UserNotification>().ToTable("UserNotifications");
+
+        // seed content here
+        new DbInitializer(modelBuilder).Seed();
+    }
+}
+
--- Modules/Db/UserManager.cs
+++ Modules/Db/UserManager.cs
@@ -1,8 +1,6 @@
-﻿namespace BasicApp.Modules.Db;
+﻿using BasicApp.Data.Models;
 
-using System.Data.Entity;
-using System.Data.Entity.ModelConfiguration.Conventions;
-using BasicApp.Data.Models;
+namespace BasicApp.Modules.Db;
 
 public class UserManager
 {
@@ -18,11 +16,12 @@ public class UserManager
     {
         try
         {
-            TestUser newPerson = new TestUser {Name=Name, Job=Job, Age=Age };
+            TestUser newPerson = new TestUser { Name = Name, Job = Job, Age = Age };
             db.TestUsers.Add(newPerson);
             db.SaveChanges();
             return true;
-        } catch { return false; }
+        }
+        catch { return false; }
     }
 
     public List<TestUser> ReadAll()
@@ -30,48 +29,3 @@ public class UserManager
         return db.TestUsers.ToList();
     }
 }
-
-public class TestDbContext : DbContext
-{
-    public DbSet<TestUser> TestUsers { get; set; }
-
-
-    public DbSet<UserNotification> UserNotifications { get; set; }
-
-
-    public TestDbContext(string ConnectionString) : base(ConnectionString)
-    {
-        
-    }
-
-    public TestDbContext() : base("postgres://postgres:00069@localhost/testdb2")
-    {
-        
-    }
-
-    public static string GetConnectionString()
-    {
-        var builder = WebApplication.CreateBuilder();
-        builder.Configuration.AddJsonFile("config/appsettings.json");
-        builder.Configuration.AddJsonFile("config/appsettings.Development.json");
-        #if DEBUG
-        string ConnectionString = builder.Configuration["Database:ConnectionStringTesting"]!;
-#else
-        string ConnectionString  = builder.Configuration["Database:ConnectionStringProduction"]!;
-#endif
-        return ConnectionString;
-    }
-
-    protected override void OnModelCreating(DbModelBuilder modelBuilder)
-    {
-        modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
-        base.OnModelCreating(modelBuilder);
-
-        modelBuilder.Entity<TestUser>().ToTable("TestUser");
-        modelBuilder.Entity<UserNotification>().ToTable("UserNotifications");
-
-        // seed content here
-        new DbInitializer(modelBuilder).Seed();
-    }
-}
-
